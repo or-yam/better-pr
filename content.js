8a2716cbb5672de1350dc09df14270c62da5d6d1
Avishay Maor <mavishay@Avishays-MacBook-Pro-16.local>
2023-06-12 11:04:14 +0300

    temp

--- content.js
+++ content.js
@@ -1,2 +1,13 @@
 // Execute code when the content script is injected into a page
 console.log("Content script loaded.");
+
+function addButtonToPage() {
+    var button = document.createElement('button');
+    button.innerText = '✨';
+    button.addEventListener('click', function() {
+      // Button click logic
+      alert('Button clicked!');
+    });
+  
+    document.body.appendChild(button);
+  }
