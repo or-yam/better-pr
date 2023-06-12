7a4d24427e24d6fca77ab605e101b9a86cf8dd4f
Oryam <48219965+or-yam@users.noreply.github.com>
2023-06-12 15:58:45 +0300

    update

--- background.js
+++ background.js
@@ -4,6 +4,26 @@ chrome.runtime.onInstalled.addListener(function () {
 });
 
 // Listen for messages from the content script
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  // Your message handling logic goes here
+chrome.runtime.onMessage.addListener(async function (message, sender, sendResponse) {
+  // if (message.action === 'fetch-data') {
+  //   const { prUrl } = message.payload;
+  //   const apiUrl = 'https://better-pr-be.deno.dev/';
+  //   try {
+  //     const response = await navigator.serviceWorker.ready.then(async swReg => {
+  //       swReg.backgroundFetch.fetch({
+  //         url: apiUrl,
+  //         method: 'GET',
+  //         headers: {
+  //           'Content-Type': 'application/json'
+  //         },
+  //         body: { prUrl }
+  //       });
+  //     });
+  //     const data = await response.json();
+  //     console.log('API Request Success:', data);
+  //     chrome.runtime.sendMessage({ action: 'return-data', payload: data });
+  //   } catch (error) {
+  //     console.error('API Request Failed:', error);
+  //   }
+  // }
 });
